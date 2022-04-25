b1886d47d54b7fec9c03e8fd9f99516006aaaf88
Daniel Matos <danikabiyesicontato@gmail.com>
2022-04-25 20:16:56 -0300

    Start Project

---
+++ app/src/main/java/com/studying/imgurchallenge/GalleryScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.studying.imgurchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GalleryScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    
+}
